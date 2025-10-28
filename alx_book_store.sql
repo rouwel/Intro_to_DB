5c11ca757e3af98fd332f16e576710d558c5d996
Rouwel <rouwelngacha@gmail.com>
2025-10-28 09:50:45 +0300

    FIrst database

---
+++ alx_book_store.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store
+
+CREATE TABLE books (book_id INT PRIMARY KEY, title VARCHAR (130), author_id INT, price DOUBLE,                                                                                                                        publication_date DATE                                                                                                                );
+
+
+
+CREATE TABLE Authors (                                                                                                                author_id INT PRIMARY KEY,                                                                                                           author_name VARCHAR (215)                                                                                                            );
+
+ 
+CREATE TABLE Customers (                                                                                                              customer_id INT PRIMARY KEY,                                                                                                         customer_name VARCHAR (215),                                                                                                         email VARCHAR (215),                                                                                                                 address TEXT                                                                                                                         );
+
+
+ CREATE TABLE Orders (                                                                                                                 order_id INT PRIMARY KEY,                                                                                                            customer_id INT,                                                                                                                     order_date DATE                                                                                                                      );
+
+ CREATE TABLE Order_Details (                                                                                                          orderdetailid INT PRIMARY KEY,                                                                                                       order_id INT,                                                                                                                        book_id INT,                                                                                                                         quantity DOUBLE                                                                                                                      );
